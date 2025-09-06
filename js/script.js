b5c6e78aeeebf18834faf427a830a65ca8c32eaa
Luis Presta <luis.presta@gmail.com>
2025-09-06 13:38:26 -0300

    web gol

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
-document.querySelector("#tema").addEventListener("change",function(){
-    document.body.classList.toggle("oscuro");
+var fotos = 20, posicion = 0;
+function foto(sentido=1,forzar=-1){
+	posicion+=sentido;
+	if(posicion>=fotos) posicion = 0;
+	if(posicion<0) posicion = fotos - 1;
+	if(forzar!=-1) posicion = forzar;
+	document.querySelector(".contenedor").style.transform = `translateX(-${5*posicion}%)`;
+}
+document.addEventListener("DOMContentLoaded",function(){
+	document.querySelector("#izquierda").addEventListener("click",function(){
+		foto(-1);
+	})
+	document.querySelector("#derecha").addEventListener("click",function(){
+		foto();
+	})
 })
